9c9d71827a3b39098e193ad19169ae07d962d026
Aristeides Stathopoulos <aristath@gmail.com>
2017-09-20 06:24:18 +0300

    initial files

---
+++ kirki-output.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Plugin Name:   Kirki Output Module
+ * Plugin URI:    http://kirki.org
+ * Description:   The CSS Output module for Kirki.
+ * Author:        Aristeides Stathopoulos
+ * Author URI:    http://aristeides.com
+ * Version:       1.0.0
+ * Text Domain:   kirki
+ *
+ * GitHub Plugin URI: aristath/kirki-output
+ * GitHub Plugin URI: https://github.com/aristath/kirki-output
+ *
+ * @package     Kirki
+ * @category    Modules
+ * @author      Aristeides Stathopoulos
+ * @copyright   Copyright (c) 2017, Aristeides Stathopoulos
+ * @license     http://opensource.org/licenses/https://opensource.org/licenses/MIT
+ * @since       1.0
+ */
+
+// Exit if accessed directly.
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
